9bf5ca115e65706857949bbbc69883c017e11403
naeemtalukder <naeemtalukder169135@gmail.com>
2023-02-25 02:19:09 +0600

    Project Done

--- Javascript/shopping.js
+++ Javascript/shopping.js
@@ -1,4 +1,51 @@
-function addToCart() 
+const cartArray = [];
+
+function displayProduct(cartProduct) {
+
+    let totalPrice = 0;
+    // console.log(cartProduct);
+    const tableBody = document.getElementById("cart-products");
+    tableBody.innerHTML = "";
+    for(let i = 0; i < cartProduct.length; i++)
+    {
+        // console.log(cartArray[i].productName, cartArray[i].productPrice);
+        const name = cartArray[i].productName;
+        const price = cartArray[i].productPrice;
+
+        totalPrice = totalPrice + price;
+
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        <th>${i + 1}</th>
+        <td>${name}</td>
+        <td>${price}</td>
+        `;
+
+        tableBody.appendChild(tr);
+    }
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+    <th></th>
+    <td>Total Price</td>
+    <td>${totalPrice}</td>
+    `;
+    tableBody.appendChild(tr);
+}
+
+function addToCart(element) 
 {
-    console.log("product clicked");
+    const productName = element.parentNode.parentNode.children[0].innerText;
+
+    const productPrice = element.parentNode.parentNode.children[1].children[0].innerText;
+
+    const productObj = {
+        productName: productName,
+        productPrice: parseFloat(productPrice)
+    }
+
+    cartArray.push(productObj);
+   
+    document.getElementById("total-added-product").innerText = cartArray.length;
+
+    displayProduct(cartArray);
 }
